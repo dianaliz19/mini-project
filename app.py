a709a74e4df51d96d7e81c01acd75679b13a2abb
Diana Liz Kuriakose <94666030+dianaliz19@users.noreply.github.com>
2024-03-16 11:58:55 +0530

    Update app.py
    updated version

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ def scrape_cet():
         text=response.content
         data=BeautifulSoup(text,'html.parser')
         events=data.find(id="lcp_instance_0")
-        return events
+        event_text = events.get_text(separator='\n')
+        return event_text.strip()
     else:
         return []
 
@@ -35,22 +36,24 @@ def scrape_nit():
         data=BeautifulSoup(text,'html.parser')
         events=data.find(class_="xc-page-column-right")
         results=events.find(class_="xc-calendar-list")
-        return results
+        event_text = results.get_text(separator='\n')
+        return event_text.strip()
     else:
         return []
 
 @app.route('/')
 def index():
+    collection.delete_many({})
     # Scrape events from College A and College B
     events_cet = scrape_cet()
     events_nit = scrape_nit()
+    print("CET events:", events_cet)
+    print("NIT events:", events_nit)
     # Store the scraped events in MongoDB
-    for event in events_cet:
-        collection.insert_one({'college': 'Cet', 'event': event})
-    for event in events_nit:
-        collection.insert_one({'college': 'nit', 'event': event})
+    collection.insert_one({'college': 'Cet', 'events': events_cet.split('\n')})
+    collection.insert_one({'college': 'nit', 'events': events_nit.split('\n')})
     # Retrieve the scraped events from MongoDB
-    scraped_data = collection.find()
+    scraped_data = collection.find({},{'_id':0})
     return render_template('index.html', scraped_data=scraped_data)
 
 if __name__ == '__main__':
